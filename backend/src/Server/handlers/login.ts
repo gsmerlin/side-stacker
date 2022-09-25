d7ed046240060fd40634ccc0c026af3cb318ac43
gsmerlin <gabriel@shopbonsai.ca>
2022-09-24 22:30:32 -0300

    Created login handler
    Very little change here, just moved it to its own file

---
+++ backend/src/Server/handlers/login.ts
@@ -0,0 +1,21 @@
+import userController from '../../Database/User'
+import { Socket } from 'socket.io'
+import { Errors, Events } from '../../enums'
+
+export interface ILogin {
+  username: string
+  password: string
+}
+
+/**
+ * Higher-order function that returns a handler for logging a user in.
+ */
+const login = (socket: Socket) =>
+  async ({ username, password: rawPassword }: ILogin) => {
+    const { data: user, error } = await userController.authenticateUser({ username, rawPassword })
+    if (error) return socket.emit(Events.LoginFailure, { error: error.message })
+    if (!user) return socket.emit(Events.LoginFailure, { error: new Error(Errors.User.NotFound) })
+    return socket.emit(Events.LoginSuccess, username)
+  }
+
+export default login
